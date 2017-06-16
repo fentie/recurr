d804428fdc3f3d1df092995c2a96370b3a83e1c4
fentie <brian.fenton@wheniwork.com>
2017-06-16 13:22:01 -0500

    adding test for date/datetime mismatch behavior for exdate

--- tests/Recurr/Test/Transformer/ArrayTransformerExDateTest.php
+++ tests/Recurr/Test/Transformer/ArrayTransformerExDateTest.php
@@ -72,6 +72,20 @@ class ArrayTransformerExDateTest extends ArrayTransformerBase
         $this->assertEquals(new \DateTime('2014-06-03 07:00:00'), $computed[1]->getStart());
     }
 
+    public function testExDateWithDateOnlyWhenStartIsDateTime()
+    {
+        $rule = new Rule(
+            'FREQ=DAILY;COUNT=3;EXDATE=20140602',
+            new \DateTime('2014-06-01 07:00:00')
+        );
+
+        $computed = $this->transformer->transform($rule);
+
+        $this->assertCount(2, $computed);
+        $this->assertEquals(new \DateTime('2014-06-01 07:00:00'), $computed[0]->getStart());
+        $this->assertEquals(new \DateTime('2014-06-03 07:00:00'), $computed[1]->getStart());
+    }
+
     public function testExDateWithMixedTimezones()
     {
         $rule = new Rule(
